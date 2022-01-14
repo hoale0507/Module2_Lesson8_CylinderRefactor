d83914be977624a5aab2d278d4340d476abaa425
hoale0507 <hoale090293@gmail.com>
2022-01-14 11:43:42 +0700

    add

--- src/com/codegym/Cylinder.java
+++ src/com/codegym/Cylinder.java
@@ -1,10 +1,21 @@
 package com.codegym;
 
 public class Cylinder {
-    public static double getVolume(int radius, int height){
+    public static double getVolume(int radius, int height) {
         double baseArea = Math.PI * radius * radius;
-        double perimeter = 2 * Math.PI  * radius;
+        double perimeter = getPerimeter(radius);
+        double volume = getVolume(height, baseArea, perimeter);
+        return volume;
+    }
+
+    private static double getVolume(int height, double baseArea, double perimeter) {
         double volume = perimeter * height + 2 * baseArea;
         return volume;
     }
+
+    private static double getPerimeter(int radius) {
+        return 2 * Math.PI * radius;
+    }
+
+
 }
